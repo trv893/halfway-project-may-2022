7c69b239ef7efa2dda66d0383c643abedd9e801b
amaddatu <>
2022-05-18 18:15:45 -0400

    Add connected js and css file

---
+++ main.js
@@ -0,0 +1,8 @@
+// select the collection of span tags from the page
+// hide them immediately in the beginning
+// add them back in using animation and a timer
+
+// expected result
+// letters will be brought in one at a time onto the page
+
+console.log("main");
