02da8c0025abdd67d504cfe7bdff739083fffc4a
amaddatu <>
2022-05-18 18:27:11 -0400

    Add finished animation

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-// select the collection of span tags from the page
-// hide them immediately in the beginning
-// add them back in using animation and a timer
+// done - select the collection of span tags from the page
+// done - hide them immediately in the beginning
+// done - add them back in using animation and a timer
+// done - center the h1 tag
 
 // expected result
 // letters will be brought in one at a time onto the page
 
 console.log("main");
+
+// animate__animated animate__bounce show
+let spanTags = $('h1 > span');
+let count = 0;
+
+let interval = setInterval(() => {
+  $(spanTags.get(count)).addClass("animate__animated animate__bounce animate__infinite show");
+
+  count++;
+  if(count >= spanTags.length){
+    clearInterval(interval);
+  }
+}, 300);
